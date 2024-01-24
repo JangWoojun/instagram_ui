c23fa4e30bc513f1ed74d9cb85cbdaf1cef36a38
JangWoojun <woojun0107@naver.com>
2024-01-25 08:05:52 +0900

    feat: 유저 스토리 영역 완료

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,52 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("home screen"),);
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          StoryArea(),
+        ],
+      ),
+    );
   }
 }
+
+class StoryArea extends StatelessWidget {
+  const StoryArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(10, (index) => UserStory(userName: "user $index"))
+      ),
+    );
+  }
+}
+
+class UserStory extends StatelessWidget {
+  final String userName;
+  const UserStory({required this.userName, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+      child: Column(
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+            decoration: BoxDecoration(
+                color: Colors.blue.shade300,
+                borderRadius: BorderRadius.circular(40)),
+          ),
+          Text(userName)
+        ],
+      ),
+    );
+  }
+}
+
