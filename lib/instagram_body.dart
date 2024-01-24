c4ef55d97df0aa0e5df69035eed538edcf21b32b
JangWoojun <woojun0107@naver.com>
2024-01-25 07:45:47 +0900

    feat: Navigation 기능 추가

--- lib/instagram_body.dart
+++ lib/instagram_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_ui/screen/home_screen.dart';
+import 'package:instagram_ui/screen/search_screen.dart';
 
 class InstagramBody extends StatelessWidget {
-  const InstagramBody({super.key});
+  final int index;
+
+  const InstagramBody({required this.index, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (index == 0) {
+      return const HomeScreen();
+    }
+    return const SearchScreen();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,42 +15,82 @@ class InstagramCloneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          colorScheme: const ColorScheme.light(
-              primary: Colors.white, secondary: Colors.black),
-          useMaterial3: true,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "Instagram",
-              style: GoogleFonts.lobsterTwo(color: Colors.black, fontSize: 32),
-            ),
-            centerTitle: false,
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.favorite_outline, size: 32),
-                onPressed: () {
-                  print("favorite_outline");
-                },
-              ),
-              IconButton(
-                icon: const Icon(CupertinoIcons.paperplane, size: 32,),
-                onPressed: () {
-                  print("paperplane");
-                },
-              ),
-            ],
-          ),
-          body: const InstagramCloneHome(),
-        ));
+            colorScheme: const ColorScheme.light(
+                primary: Colors.white, secondary: Colors.black),
+            useMaterial3: true,
+            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              selectedItemColor: Colors.black,
+            )),
+        home: const InstagramCloneHome());
   }
 }
 
-class InstagramCloneHome extends StatelessWidget {
+class InstagramCloneHome extends StatefulWidget {
   const InstagramCloneHome({super.key});
 
   @override
+  State<InstagramCloneHome> createState() => _InstagramCloneHomeState();
+}
+
+class _InstagramCloneHomeState extends State<InstagramCloneHome> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return InstagramBody();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Instagram",
+          style: GoogleFonts.lobsterTwo(color: Colors.black, fontSize: 32),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite_outline, size: 32),
+            onPressed: () {
+              print("favorite_outline");
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.paperplane,
+              size: 32,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: InstagramBody(index: index),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (newIndex) {
+          setState(() {
+            index = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 28,
+              ),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                size: 28,
+              ),
+              label: "Search"),
+        ],
+      ),
+    );
   }
 }
